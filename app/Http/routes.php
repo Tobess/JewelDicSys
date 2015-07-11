d27827c99462c0dfba24e42dd987b191d0456601
Tobess <Jontor@foxmail.com>
2015-07-11 20:20:57 +0800

    Suport to update pinyin and letter.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -39,8 +39,8 @@ Route::group(['namespace' => 'Console', 'prefix' => 'console'], function()
 });
 
 // 内网公开接口
-Route::group(['domain' => env('APP_DEBUG', false) ? 'dictionary.app' : '192.168.1.11'], function()
-{
+//Route::group(['domain' => env('APP_DEBUG', false) ? 'dictionary.app' : '192.168.1.11'], function()
+//{
     Route::get('analyse/{identify}', 'HomeController@getAnalyse');
     Route::controller('resource', 'ResourcesController');
-});
+//});
