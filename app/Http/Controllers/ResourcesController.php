4b9a7cf25f0e291be62a74d03aca0d8f853d30dc
Tobess <Jontor@foxmail.com>
2016-12-12 17:54:15 +0800

    测试接口。

--- app/Http/Controllers/ResourcesController.php
+++ app/Http/Controllers/ResourcesController.php
@@ -389,18 +389,20 @@ class ResourcesController extends Controller {
 
         $countries = [];
         if ($city && $district) {
-            $ppid = \App\Area::where('level', '2')->where(function ($sql) use ($city) {
+            $ppid = \App\Area::where('level', 2)->where(function ($sql) use ($city) {
                 $sql->where('name', 'like', '%'.$city.'%')
                     ->orWhere('short_name', 'like', '%'.$city.'%');
             })->lists('id');
+            \Log::info(print_r($ppid, true));
             if (count($ppid) > 0) {
                 $did = \App\Area::where('level', '3')
                     ->where(function ($sql) use ($district) {
                         $sql->where('name', 'like', '%' . $district . '%')
                             ->orWhere('short_name', 'like', '%' . $district . '%');
                     })
-                    ->whereRaw("FIND_IN_SET(parent_id, '{".implode(',', $ppid)."}')")
+                    ->whereIn('parent_id', $ppid)
                     ->pluck('id');
+                \Log::info(print_r($did, true));
                 if ($did > 0) {
                     $countries = \App\Area::where('level', 4)->where('parent_id', $did)
                         ->select('id', 'name')->get();
