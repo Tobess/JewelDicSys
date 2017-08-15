de3a9b491ffb946d874625ba29a621564efe213b
Tobey Ho <ho@fromai.com>
2017-08-15 14:10:34 +0800

    测试自动拆分

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -76,6 +76,8 @@ class HomeController extends Controller {
                 $sKey = $redisIdentify.':status';
                 $redis->del($sKey);
 
+                Log::info("####################################################################################");
+
                 foreach ($gNameArr as $gName) {
                     // 商品名称拆分队列
                     \Queue::push(function($job) use ($gName, $redisIdentify, $sKey, $redis)
@@ -83,9 +85,11 @@ class HomeController extends Controller {
                         $gNameKey = $redisIdentify . ':'. md5($gName);
                         // S1 生成商品名称全拼码
                         $pinyin = \App\Word::getPinyinAndCache($gName);
+                        Log::info($pinyin);
 
                         // S2 拆分分析
                         $results = \App\Word::search($pinyin);
+                        Log::info(print_r($results, true));
                         if (isset($results['words']) && count($results['words'])) {
                             $redis->set($gNameKey, json_encode($results));
                             $redis->expire($gNameKey, 24*60*60);
